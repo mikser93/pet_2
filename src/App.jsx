7c245924fbe380eeb7e821817f56da3c69c66361
Vova Vykhrystenko <mikserwowa@gmail.com>
2022-10-25 11:52:33 +0300

    Del files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,1 @@
-import { EventBoard } from 'components/EventBoard/EventBoard';
-import { PageTitle } from 'components/PageTitle/PageTitle';
-import events from 'events.json';
 
-export function App() {
-  return (
-    <>
-      <PageTitle text="24 core worlds conference" />
-      <EventBoard events={events} />
-    </>
-  );
-}
